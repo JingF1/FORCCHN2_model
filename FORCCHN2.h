98fcab448b5ccd5325d9581dee9f26932df50dd5
JingF1 <97280535+JingF1@users.noreply.github.com>
2022-01-13 15:24:03 +0800

    Delete FORCCHN2.h

--- FORCCHN2.h
+++
@@ -1,2 +0,0 @@
-// matlab call fotran DLL (header file)
-void forcchn2(int*,float*,float*,float*,int*,int*,int*,int*,float*,float*,float*,float*,float*,float*,float*,float*,float*,float*,float*,float*,float*,float*,float*,float*,float*,float*,int*,float*,float*,float*,float*);
